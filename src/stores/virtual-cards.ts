738a6242848b424bcace37ddf5d5f18a691d4fcf
david-bfg <david.1.bfg@gmail.com>
2023-05-28 13:00:06 -0500

    Added card getter and modifiers (edit & add)

--- src/stores/virtual-cards.ts
+++ src/stores/virtual-cards.ts
@@ -19,6 +19,7 @@ interface card {
 
 export const useVirtualCards = defineStore('virtual-cards', () => {
   const virtualCards = ref<Array<card>>([]);
+  const mapCards = new Map<string, number>();
   const apiKeyToken = 'lithic-key-token';
   // const account_token = env.account_token;
 
@@ -43,20 +44,93 @@ export const useVirtualCards = defineStore('virtual-cards', () => {
       api
         .request(options)
         .then(function (response) {
-          console.log(response.data);
-          console.log(response.data.data);
           virtualCards.value = response.data.data;
           // TODO: add case for multiple pages.
+          mapCards.clear();
+          virtualCards.value.forEach((card, i) => mapCards.set(card.token, i))
         })
         .catch(function (error) {
           console.error(error);
         });
     } else {
       virtualCards.value = [];
+      mapCards.clear();
     }
+  }
+
+  async function editCard(cardToken: string, spend_limit_duration: string, spend_limit: number, memo: string, state: string) {
+    const apiKey = localStorage.getItem(apiKeyToken) || '';
+    const card = getCard(cardToken);
+    const edits = {spend_limit_duration, spend_limit, memo, state};
+    Object.keys(card).forEach(key => {
+      if(card[key] === edits[key]) delete edits[key];
+    });
+
+    if(apiKey.length){
+      const options = {
+        method: 'PATCH',
+        url: '/lithic/cards/' + cardToken,
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json',
+          Authorization: 'api-key ' + apiKey
+        },
+        data: edits
+      };
+  
+      api
+        .request(options)
+        .then(function (response) {
+          Object.keys(card).forEach(key => card[key] = response.data[key]);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    } else {
+      console.error('error: No API Key');
+    }
+  }
+
+  async function addCard(spend_limit_duration: string, spend_limit: number, memo: string, state: string) {
+    const apiKey = localStorage.getItem(apiKeyToken) || '';
+
+    if(apiKey.length){
+      const options = {
+        method: 'POST',
+        url: '/lithic/cards/',
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json',
+          Authorization: 'api-key ' + apiKey
+        },
+        data: {
+          type: 'VIRTUAL',
+          spend_limit_duration: spend_limit_duration,
+          spend_limit: spend_limit,
+          memo: memo,
+          state: state
+        }
+      };
+      
+      api
+        .request(options)
+        .then(function (response) {
+          virtualCards.value.push(response.data);
+          mapCards.set(response.data.token, virtualCards.value.length-1);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    } else {
+      console.error('error: No API Key');
+    }
+  }
+
+  function getCard(token: string) {
+    return virtualCards.value?.[mapCards.get(token)];
   }
 
   refresh();
 
-  return { virtualCards, refresh };
+  return { virtualCards, refresh, getCard, editCard, addCard };
 });
