4f5e8126ecd3c4a7201ca64109e57c47693ab251
david-bfg <david.1.bfg@gmail.com>
2023-06-10 11:57:05 -0500

    implement persistent state for subscrition store new ref linking a subscrition to a payment card. The state is persited using pinia plugin. calc map to get sub data from card token. added helper fuctions for interfacing with linking reference. getSubFromCard: from a card token grab the subscription it is linked to setSubLink: create new link between card and subscription getLinkedCardToken: from a subscription id grab the card token it is linked to

--- src/stores/user-subscriptions.ts
+++ src/stores/user-subscriptions.ts
@@ -9,19 +9,75 @@ export interface sub {
   price: number;
   period: string;
   service_id: string;
-  card?: boolean;
+}
+
+export interface subLink {
+  sub: sub;
+  cardToken?: string;
 }
 
 export const useUserSubscriptions = defineStore('user-subscriptions', () => {
   const userSubscriptions = ref<Array<sub>>([]);
+  // TODO: load on startup and persist data
+  const subLinksToVirtualCards = ref(new Map<string, subLink>());
+  // TODO: calculate on startup
+  const cardToSub = new Map<string, sub>();
 
   async function refresh() {
     if(meteorServerSock.userId){
-      const streamManager = await meteorServerSock.call('subscription.details');
-      userSubscriptions.value = streamManager;
+      userSubscriptions.value = await meteorServerSock.call('subscription.details');
+      userSubscriptions.value.forEach(sub => {
+        const id = 'w2w:' + sub.service_id;
+        const value = subLinksToVirtualCards.value.get(id);
+        if(value){
+          value.sub = sub;
+          if(value.cardToken){
+            cardToSub.set(value.cardToken, sub);
+          }
+        } else {
+          subLinksToVirtualCards.value.set(id, {sub: sub});
+        }
+      });
     } else {
       userSubscriptions.value = [];
     }
   }
-  return { userSubscriptions, refresh };
-});
+
+  function getSubFromCard(token: string) {
+    return cardToSub.get(token);
+  }
+
+  function setSubLink(service_id: string, cardToken: string) {
+    const id = 'w2w:' + service_id;
+    const subLnk = subLinksToVirtualCards.value.get(id);
+    if(subLnk){
+      subLnk.cardToken = cardToken;
+      cardToSub.set(cardToken, subLnk.sub);
+    } else {
+      throw new Error('Error: Subscription service not found');
+    }
+  }
+
+  function getLinkedCardToken(service_id: string) {
+    return subLinksToVirtualCards.value.get('w2w:' + service_id)?.cardToken;
+  }
+
+  return {
+    userSubscriptions,
+    subLinksToVirtualCards,
+    refresh,
+    getSubFromCard,
+    setSubLink,
+    getLinkedCardToken
+  };
+},
+{
+  persist: {
+    afterRestore: (ctx) => {
+      ctx.store.subLinksToVirtualCards = new Map<string, subLink>(
+          Object.entries(ctx.store.subLinksToVirtualCards));
+    },
+    paths: ['subLinksToVirtualCards'],
+  },
+},
+);
