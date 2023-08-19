18a6b1828697d24cd58afdfdb57a1479cc1e61e8
david-bfg <david.1.bfg@gmail.com>
2023-08-19 14:15:05 -0500

    main app layout with tabs & IonRouterOutlet changed base app to use ionic library theres is a header with some menu buttons and tabs for main app navigation
    IonRouterOutlet is used and configured with what would be intended to
    describe a basic outline of how the app is meant to be navigated
    
    max width and centering added to css to make destop use and development easier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,148 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./routes/Login";
+import LithicAPI from "./routes/LithicAPI";
+import VirtualCards from "./routes/VirtualCards";
+import Subscriptions from "./routes/Subscriptions";
+import CardEditor from "./routes/CardEditor";
+
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  setupIonicReact,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonButtons,
+  IonButton,
+  IonMenu,
+  IonMenuToggle,
+  IonPage,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+
+import { Route, Redirect } from "react-router";
+
+import {
+  cardOutline,
+  logInOutline,
+  walletOutline,
+  codeSlashOutline,
+  settings,
+  logIn,
+  informationCircle,
+} from "ionicons/icons";
+
+setupIonicReact();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="primary">
+              <IonButton>
+                <IonIcon
+                  slot="end"
+                  icon={informationCircle}
+                  size="large"
+                ></IonIcon>
+              </IonButton>
+            </IonButtons>
+            <IonTitle>Service Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <Login />
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonButtons slot="start">
+              <IonMenuToggle>
+                <IonButton>
+                  <IonIcon slot="start" icon={logIn} size="large"></IonIcon>
+                  Login
+                </IonButton>
+              </IonMenuToggle>
+            </IonButtons>
+            <IonTitle>
+              Sub-Troll <br /> Gain controll of your subscriptions
+            </IonTitle>
+            <IonButtons slot="end">
+              <IonButton>
+                <IonIcon slot="end" icon={settings} size="large"></IonIcon>
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonReactRouter>
+            <IonTabs>
+              <IonRouterOutlet>
+                <Redirect exact path="/" to="/login" />
+                {/*
+                  Use the render method to reduce the number of renders your component will have due to a route change.
+
+                  Use the component prop when your component depends on the RouterComponentProps passed in automatically.
+                */}
+                <Route path="/login" render={() => <Login />} exact={true} />
+                <Route
+                  path="/subscriptions"
+                  render={() => <Subscriptions />}
+                  exact={true}
+                />
+                <Route
+                  path="/subscriptions/card-edit/:id"
+                  render={() => <CardEditor />}
+                />
+                <Route
+                  path="/subscriptions/card-add/:id"
+                  render={() => <CardEditor />}
+                />
+                <Route
+                  path="/cards"
+                  render={() => <VirtualCards />}
+                  exact={true}
+                />
+                <Route path="/cards/edit/:id" render={() => <CardEditor />} />
+                <Route
+                  path="/lithic"
+                  render={() => <LithicAPI />}
+                  exact={true}
+                />
+              </IonRouterOutlet>
+
+              <IonTabBar slot="bottom">
+                <IonTabButton tab="login" href="/login">
+                  <IonIcon icon={logInOutline} />
+                  <IonLabel>Login</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="subscriptions" href="/subscriptions">
+                  <IonIcon icon={walletOutline} />
+                  <IonLabel>Subscriptions</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="virtual-cards" href="/cards">
+                  <IonIcon icon={cardOutline} />
+                  <IonLabel>Virtual Cards</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="lithic-api" href="/lithic">
+                  <IonIcon icon={codeSlashOutline} />
+                  <IonLabel>Lithic API</IonLabel>
+                </IonTabButton>
+              </IonTabBar>
+            </IonTabs>
+          </IonReactRouter>
+        </IonContent>
+      </IonPage>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
