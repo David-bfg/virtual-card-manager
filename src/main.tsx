414446aa4e37ab2a2ee5dffd2832852dd9137e92
david-bfg <david.1.bfg@gmail.com>
2023-08-19 14:11:22 -0500

    import ionic + css and attempt login at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { IonApp } from "@ionic/react";
+import meteorServerSock from "./assets/lib/meteor-sock";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css"; // Remove if nothing is visible
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+const what2watchlistToken = "w2w-token";
+const token = localStorage.getItem(what2watchlistToken);
+if (token) {
+  meteorServerSock.login({ resume: token }).catch((e: Error) => {
+    // TODO: send to login page
+    console.error(
+      "Stored login token failed." + " Unable to re-initiate login.",
+      e
+    );
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <IonApp>
+      <App />
+    </IonApp>
+  </React.StrictMode>
+);
